826fa0c8d1c4d128dadd23404e5a962205448058
jobrauer <jobrauer@redhat.com>
2021-11-09 19:28:22 +0100

    Tweak deployment scripts

--- hydroserver.py
+++ hydroserver.py
@@ -12,18 +12,24 @@ def make_shell_context():
     return {'db': db, 'Device': Device, 'cache': CACHE}
 
 
-args = sys.argv
-if 'run' in args:
-    # initialize plug-in tasks
-    # from app.core.plugins import PLUGIN_MANAGER
-    # PLUGIN_MANAGER.initialize(app.config['PLUGIN_PATHS'])
+def init():
+    args = sys.argv
+    if 'db' in args:
+        return
+
+    if 'run' in args:
+        pass
 
     # Start-up: loading (health-check) devices from database, caching
     from app.system.device_controller import refresh_devices
     refresh_devices()
+
+    # refresh/import configured systems/properties
     from app.grow.models import load_from_lists
     load_from_lists(
         properties=app.config.get("DEFAULT_GROW_PROPERTIES"),
         systems=app.config.get("DEFAULT_SYSTEMS")
     )
 
+
+init()
